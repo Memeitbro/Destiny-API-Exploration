5220292ef7565174c0d4dadc8d34deaaea2c4757
Martin <m.martin23803@gmail.com>
2024-05-03 13:54:14 +0200

    feat: implement new solution

---
+++ Destiny_API_Exploration/Destiny_API_Exploration/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Destiny_API_Exploration;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
